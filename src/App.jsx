03c50d6cd66cfd411fb202b51442dc1b73c05503
RafaelFF6844 <hiji.jr@gmail.com>
2024-06-27 15:53:34 -0400

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='nav'>
+        <img className='nav-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png' />
+        <h2 className='nav-title'>Pokedex -H</h2>
+
+        <div className='nav-items'>
+          <div>This project</div>
+          <div>About me</div>
+        </div>
+      </div>
+
+      <div className='content'>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default App
